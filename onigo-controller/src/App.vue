f2abde9f83ca4688bbd194c55b19495c52c11350
shundroid <shundroid@users.noreply.github.com>
2016-07-19 18:52:57 +0900

    inactive screenを追加

--- onigo-controller/src/App.vue
+++ onigo-controller/src/App.vue
@@ -1,35 +1,20 @@
 <template>
-  <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <hello></hello>
-    <p>
-      Welcome to your Vue.js app!
-    </p>
-    <p>
-      To get a better understanding of how this boilerplate works, check out
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">its documentation</a>.
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
-    </p>
-  </div>
+  <inactive-screen :message="inactiveMessage"></inactive-screen>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import InactiveScreen from "./components/InactiveScreen";
 
 export default {
+  data () {
+    return {
+      inactiveMessage: "This shows when the game is inactive."
+    };
+  },
   components: {
-    Hello
+    InactiveScreen
   }
-}
+};
 </script>
 
 <style>
@@ -38,27 +23,12 @@ html {
 }
 
 body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  background-color: #34495e;
+  color: white;
+  margin: 0px;
   height: 100%;
-}
-
-#app {
-  color: #2c3e50;
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
-  text-align: center;
-}
-
-#app a {
-  color: #42b983;
-  text-decoration: none;
-}
-
-.logo {
-  width: 100px;
-  height: 100px
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
 }
 </style>
---
+++ onigo-controller/src/components/InactiveScreen.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="inactive-screen">
+    <div id="inactive-inner">
+      {{ message }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: String
+  }
+};
+</script>
+
+<style>
+#inactive-screen {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 5;
+  background-color: rgba(0, 0, 0, 0.6);
+}
+
+#inactive-inner {
+  width: 100%;
+  text-align: center;
+  background-color: black;
+  color: white;
+  position: absolute;
+  top: 50%;
+  padding: 20px 0;
+  transform: translateY(-50%);
+}
+</style>
